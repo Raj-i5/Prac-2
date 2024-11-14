22aa7dc252fbb17dca30f6d72ec7f6b97db3be1e
Raj-i5 <raj2.0.ai.ml@gmail.com>
2024-11-14 12:13:54 +0530

    Raj

---
+++ por-4.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from datetime import datetime
+import numpy as np
+range_date = pd.date_range(start ='1/10/2023', end ='31//10/2023',freq ='Min')
+print(range_date)
+print (type([0]))
